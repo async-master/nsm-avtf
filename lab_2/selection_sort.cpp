fad4ce8526038eb3bc1b3b9d947a01a3dd07292b
Nikita M <125992442+async-master@users.noreply.github.com>
2023-03-14 17:13:05 +0700

    Update selection_sort.cpp
    fin version

--- lab_2/selection_sort.cpp
+++ lab_2/selection_sort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 
-
 void selection_sort(float *arr, int size)
 {
     for (int i = 0; i < size - 1; i++)
@@ -9,10 +8,7 @@ void selection_sort(float *arr, int size)
         int min_index = i;
         for (int j = i + 1; j < size; j++)
         {
-            if (arr[j] < arr[min_index])
-            {
-                min_index = j;
-            }
+            if (arr[j] < arr[min_index]) min_index = j;
         }
         if (min_index != i)
         {
@@ -33,10 +29,11 @@ int main(){
 	    }
 	    randomDigits[i] = 0.01 * (rand() % 101);
 	}
+	std::cout <<"Не отсортированный массив: " << std::endl;
 	for(int i = 0; i < size; i++) std::cout << randomDigits[i] << " ";
 	std::cout << std::endl;
 	selection_sort(randomDigits,size);
+	std::cout <<"Отсортированный массив: " << std::endl;
 	for(int i = 0; i < size; i++) std::cout << randomDigits[i] << " ";
 	std::cout << std::endl;
-	
 }
