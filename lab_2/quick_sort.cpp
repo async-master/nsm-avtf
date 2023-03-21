564c408e507e2c4750bb8d2a8c717be727bb4821
Nikita M <125992442+async-master@users.noreply.github.com>
2023-03-21 17:26:34 +0700

    Update quick_sort.cpp

--- lab_2/quick_sort.cpp
+++ lab_2/quick_sort.cpp
@@ -28,7 +28,7 @@ void quickSort(float *arr, int low, int high){
 int main()
 {   
     int size; std::cout << "Введите размер массива: "; std::cin >> size;
-	float randomDigits[size] {};
+	float *randomDigits = new float[size];
 	for(int i = 0; i < size; i++){
 	    if(i%2 == 1){
 	        randomDigits[i] = -0.01 * (rand() % 101);
