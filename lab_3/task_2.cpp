b6bd8b6d96e1f071a2f2c5243ee68dab70b36420
Nikita M <125992442+async-master@users.noreply.github.com>
2023-03-24 18:58:32 +0700

    Update task_2.cpp

--- lab_3/task_2.cpp
+++ lab_3/task_2.cpp
@@ -3,20 +3,17 @@
 int main()
 {
 	std::cout<<"Введите порядок матрицы: "; int n; std::cin>>n;
-	
 	int** matrix = new int*[n];
-
 	for (int i = 0; i < n; i++) {
         matrix[i] = new int[n];
-    }
-
-	int c =1;
+    	}
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
-			matrix[i][j] = c++;
+			matrix[i][j] = rand()%100;
 		}
 	}
 	std::cout << std::endl;
+	
 	
 	std::cout<<"Исходная матрица: " << std::endl;
 	for(int i=0;i<n;i++){
@@ -27,6 +24,7 @@ int main()
 	}
 	std::cout << std::endl;
 
+	
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			matrix[i][j] = matrix[i][j] * ((i+j)/(n-1));
@@ -34,6 +32,7 @@ int main()
 	}
 	matrix[n-1][n-1] = matrix[n-1][n-1]/2;
 
+	
 	std::cout<<"Обнуление элементов матрицы выше побочной диагонали матрицы: " << std::endl;
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
@@ -41,6 +40,7 @@ int main()
 		}
 		std::cout << std::endl;
 	}
+	
 	
 	for (int i = 0; i < n; i++) 
 		delete[] matrix[i];
