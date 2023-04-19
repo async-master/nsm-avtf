1ebfe40ca1fee67ac45034be59206780217960d1
Nikita M <125992442+async-master@users.noreply.github.com>
2023-04-19 17:19:21 +0700

    Create task_7.cpp

---
+++ lab_4/task_7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+bool check_symbol(char symbol, std::string* str){
+    for(int i=0;i<str.length;i++){
+        
+    }
+    return ch;
+}
+
+int main()
+{
+    std::string str1 = "abbcccddd";
+    std::string str2 = "aaaabbbccd";
+    
+    const int len1 = str1.length();
+    const int len2 = str2.length();
+
+    std::string str3 = ""; // конечная строка.
+    std::string str4 = ""; // символы которые уже были.
+       
+    for(int i=0;i<len1;i++){
+        
+    }   
+    
+    std::cout << str;
+
+
+    return 0;
+}
