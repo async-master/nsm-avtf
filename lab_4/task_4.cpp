65fe8130168760a22fc9bea4cbcc1c4e1574dc7a
Nikita M <125992442+async-master@users.noreply.github.com>
2023-04-29 16:49:42 +0700

    Update task_4.cpp

--- lab_4/task_4.cpp
+++ lab_4/task_4.cpp
@@ -2,24 +2,25 @@
 #include <string>
 
 int vowel_count(std::string str){
-    int n=0; //counting vowel
+    int n=0;
     for(int i=0; i<=str.length();i++){
-        if(int(str[i])==97||int(str[i])==165||int(str[i])==168||int(str[i])==174||int(str[i])==227||int(str[i])==235||int(str[i])==237||int(str[i])==238||int(str[i])==239||int(str[i])==241) n++;
+        if(int(str[i])==97||int(str[i])==101||int(str[i])==105||int(str[i])==111||int(str[i])==117||int(str[i])==121) n++;
     }
     return n;
 }
 
-void insertion_sort(std::string *arr, int size){
+void insertion_sort_vowel(std::string *arr, int size){
 	for(int i=0;i<size;i++){
 		std::string x=arr[i];
 		int j=i-1;
-		while(j>=0 && vowel_count(arr[j])>=vowel_count(x)){
+		while(j>=0 && vowel_count(arr[j])>=vowel_count(x) && int(arr[j][0])<=int(x[0])){
 			arr[j+1]=arr[j];
 			j--;
 		}
 		arr[j+1]=x;
 	}
 }
+
 
 int main()
 {
@@ -31,9 +32,9 @@ int main()
         std::cin >> arr[i];
     }
     
-    insertion_sort(arr, word_count);
+    insertion_sort_vowel(arr, word_count);
 
-    for(int i=0; i<word_count;i++){
+    for(int i=word_count; i>=0;i--){
         std::cout << arr[i] << " ";
     }
     
