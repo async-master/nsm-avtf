78cb29c9be26cb91905d6d3dad31ed903ead6252
Nikita M <125992442+async-master@users.noreply.github.com>
2023-05-06 20:38:26 +0700

    Update task_5.cpp

--- basics_of_programming_and_algorithmization/lab_4/task_5.cpp
+++ basics_of_programming_and_algorithmization/lab_4/task_5.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+
+int word_count(std::string file_name){
+    std::ifstream file;
+    file.open(file_name);
+    
+    if(file.is_open()){
+        int count = 1;
+        std::string str;
+        getline(file,str);
+        
+        for (int i = 0; i < str.size(); i++)
+            if (str[i] == ' ') count++;
+            
+        file.close();
+        return count;
+    }
+    else{
+        file.close();
+    }
+    return 0;
+}
 
 void insertion_sort_len_word(std::string *arr, int size){
 	for(int i=0;i<size;i++){
@@ -13,23 +35,31 @@ void insertion_sort_len_word(std::string *arr, int size){
 	}
 }
 
-
-
 int main()
 {
-    int word_count; std::cout << "Введите кол-во слов, которое вы хотите ввести: "; std::cin >> word_count;
+    std::string file_name; std::cout << "Input file name: "; std::cin >> file_name;
+    int words = word_count(file_name);
     
-    std::string arr[word_count];
+    std::string arr[words];
     
-    for(int i=0; i<word_count;i++){
-        std::cin >> arr[i];
-    }
+    std::ifstream file;
+    file.open(file_name);
     
-    insertion_sort_len_word(arr, word_count);
+    if(file.is_open()){
+    
+        for(int i=0; i<words;i++){
+            file >> arr[i];
+        }
+    
+        insertion_sort_len_word(arr, words);
 
-    for(int i=word_count; i>=0;i--){
+        for(int i=words; i>=0;i--){
         std::cout << arr[i] << " ";
-    }
+        }
     
+        return 0;
+    }else{
+        file.close();
+    }
     return 0;
 }
