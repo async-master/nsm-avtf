42352971c8e37611b42bc8e361447124482228c7
Nikita M <125992442+async-master@users.noreply.github.com>
2023-05-06 20:59:20 +0700

    Update task_6.cpp

--- basics_of_programming_and_algorithmization/lab_4/task_6.cpp
+++ basics_of_programming_and_algorithmization/lab_4/task_6.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main()
 {
-    std::string str = "##Hel##lo worl##d##";
-    const int length = str.length();
+    std::string file_name; std::cout << "Input file name: "; std::cin >> file_name;
+    
+    std::ifstream file;
+    file.open(file_name);
+    
+    if(file.is_open()){
+    
+        std::string str;
+        getline(file,str);
+        const int length = str.length();
 
-    int n = 0;
-    for (int i = 0; i < length; i++) {
-        if (str.at(i) == '#') n++;
-    }
-    bool ch = false;
-    for(int i = 0; i < length-n/2; i++) {
-        if (str.at(i) == '#' && str.at(i+1) == '#') {
-            if(!ch){
-                std::string str1(str, 0, i);
-                std::string str2(str, i+2, length+1);
-                str.clear();
-                str.append(str1).append("<").append(str2);
-                ch = true;
-                continue;
-            }
-            if(ch){
-                std::string str1(str, 0, i);
-                std::string str2(str, i+2, length+1);
-                str.clear();
-                str.append(str1).append(">").append(str2);
-                ch = false;
-                continue;
+        int n = 0;
+        for (int i = 0; i < length; i++) {
+            if (str.at(i) == '#') n++;
+        }
+        bool ch = false;
+        for(int i = 0; i < length-n/2; i++) {
+            if (str.at(i) == '#' && str.at(i+1) == '#') {
+                if(!ch){
+                    std::string str1(str, 0, i);
+                    std::string str2(str, i+2, length+1);
+                    str.clear();
+                    str.append(str1).append("<").append(str2);
+                    ch = true;
+                    continue;
+                }
+                if(ch){
+                    std::string str1(str, 0, i);
+                    std::string str2(str, i+2, length+1);
+                    str.clear();
+                    str.append(str1).append(">").append(str2);
+                    ch = false;
+                    continue;
+                }
             }
         }
-    }
        
-    std::cout << str;
-    
+        std::cout << str;
+    }else{
+        std::cout<<"ERROR"<<std::endl;
+    }
     return 0;
 }
