55a879cc9307e24b511d9e95697bcd24a6151e98
Nikita M <125992442+async-master@users.noreply.github.com>
2023-05-06 20:23:31 +0700

    Update task_3.cpp

--- basics_of_programming_and_algorithmization/lab_4/task_3.cpp
+++ basics_of_programming_and_algorithmization/lab_4/task_3.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
-#include <iterator>
-#include <string>
+#include <fstream>
+
 int main()
 {
-    std::string str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    const int length{ static_cast<int>(std::size(str)) };
-
-    int n = 0;
-    for (int i = 0; i < length; i++) {
-        if (str.at(i) == 'a') n++;
-    }
-
-    for(int i = 0; i < length+n; i++) {
-        if (str.at(i) == 'a') {
-            std::string str1(str, 0, i+1);
-            std::string str2(str, i+1, length+1);
-            str.clear();
-            str.append(str1).append("b").append(str2);
-            
-            
+    std::ifstream file;
+    file.open("text.txt");
+    
+    if(file.is_open()){
+        
+        std::string str;
+        getline(file,str);
+        const int len = str.length();
+        int n = 0;
+        
+        for (int i = 0; i < len; i++) {
+            if (str.at(i) == 'a') n++;
         }
-    }
-       
-    std::cout << str;
 
+        for(int i = 0; i < len+n; i++) {
+            if (str.at(i) == 'a') {
+                std::string str1(str, 0, i+1);
+                std::string str2(str, i+1, len+1);
+                str.clear();
+                str.append(str1).append("b").append(str2);
+            }
+        }
+        
+        std::cout << str;
+    }
+    file.close();
 
     return 0;
 }
