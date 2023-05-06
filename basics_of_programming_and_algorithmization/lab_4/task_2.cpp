efdb6c9063344ddb970631cc1920e550057d2666
Nikita M <125992442+async-master@users.noreply.github.com>
2023-05-06 22:25:25 +0700

    Update task_2.cpp

--- basics_of_programming_and_algorithmization/lab_4/task_2.cpp
+++ basics_of_programming_and_algorithmization/lab_4/task_2.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-#include <iterator>
+#include <fstream>
+
 int main()
 {
-    char arr[]{"abababababbbbabaaaaaaaabbbbaabababababbbb"};
-    const int length{ static_cast<int>(std::size(arr)) };
-    int n = 0;
-    for (int i = 0; i <= length-1; i++) {
-        if (arr[i] == 'a') arr[i] = 'A';
-        if (arr[i] == 'b') arr[i] = 'B';
+    std::string file_name; std::cout << "Input file name: "; std::cin >> file_name;
+    
+    std::ifstream file;
+    file.open(file_name);
+    
+    if(file.is_open()){
+        
+        std::string str;
+        getline(file,str);
+        const int len=str.length();
+        char arr[len];
+        for(int i=0;i<len;i++){
+            arr[i]=str[i];
+        }
+        int n = 0;
+        for (int i = 0; i <= len-1; i++) {
+            if (arr[i] == 'a') arr[i] = 'A';
+            if (arr[i] == 'b') arr[i] = 'B';
+        }
+        std::cout << arr;
+    }else{
+        std::cout << "ERROR"<<std::endl;
     }
-    std::cout << arr;
-
     return 0;
 }
