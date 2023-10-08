d535f5ec43f73b7fd1d9aebc3f03c1d19f5b1367
Nikita M <125992442+async-master@users.noreply.github.com>
2023-10-08 23:54:26 +0700

    Update Matrix.cpp

--- OOP/Matrix.cpp
+++ OOP/Matrix.cpp
@@ -2,21 +2,25 @@
 #include<iostream>
 #include<vector>
 
-void Matrix::printSize() {
-    std::cout << "rows: " << this->rows << "; " << "colms: " << this->colms << "; \n";
-}
-
-void Matrix::fullIn() {
-    m.resize(rows);
-    for (int i = 0; i < rows; i++) {
-        m[i].resize(colms);
-        for (int j = 0; j < colms; j++) {
-            m[i][j] = rand() % 10;
+Matrix::Matrix(std::string str) {
+    std::ifstream in(str); // окрываем файл для чтения
+    if (in.is_open()) {
+        in >> rows >> colms;
+        m.resize(rows);
+        for (int i = 0; i < rows; i++) {
+            m[i].resize(colms);
+            for (int j = 0; j < colms; j++) {
+                in >> m[i][j];
+            }
         }
     }
+    else {
+        std::cout << "Попробуйте ещё раз. \n";
+    }
+    in.close();
 }
 
-void Matrix::printSize() {
+void Matrix::printMatrix() {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < colms; j++) {
             std::cout << m[i][j] << " ";
@@ -25,7 +29,7 @@ void Matrix::printSize() {
     }
 }
 
-void Matrix::miltOnNum(int num) {
+void Matrix::miltOnNum(double num) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < colms; j++) {
             m[i][j] *= num;
@@ -33,22 +37,66 @@ void Matrix::miltOnNum(int num) {
     }
 }
 
-void Matrix::resize(int rows, int colms) {
-    m.resize(rows);
-    for (int i = 0; i < rows; i++) {
-        m[i].resize(colms);
-    }
-}
-
 Matrix Matrix::transp() {
     Matrix temp(colms, rows);
-    temp.resize(colms, rows);
-
-    for (int i = 0; i < colms; i++) {
-        temp[i].resize(rows);
-        for (int j = 0; j < rows; j++) {
-            temp[i][j] = m[j][i];
+    temp.m.resize(temp.rows);
+    for (int i = 0; i < temp.rows; i++) {
+        temp.m[i].resize(temp.colms);
+        for (int j = 0; j < temp.colms; j++) {
+            temp.m[i][j] = m[j][i];
         }
     }
     return temp;
+}
+
+double Matrix::det(std::vector<std::vector<double>> m,int n) {
+    double num1, num2, det = 1, index, total = 1;
+    int* temp{ new int[n+1] };
+    for (int i = 0; i < n; i++)
+    {
+        index = i;
+        while (index < n && m[index][i] == 0)  index++;
+        if (index == n)continue;
+        if (index != i){
+            for (int j = 0; j < n; j++){
+                int temp = m[index][j];
+                m[index][j] = m[i][j];
+                m[i][j] = temp;
+            }
+            det = det * pow(-1, index - i);
+        }
+        for (int j = 0; j < n; j++){
+            temp[j] = m[i][j];
+        }
+        for (int j = i + 1; j < n; j++){
+            num1 = temp[i];
+            num2 = m[j][i];
+            for (int k = 0; k < n; k++){
+                m[j][k]
+                    = (num1 * m[j][k]) - (num2 * temp[k]);
+            }
+            total = total * num1;
+        }
+    }
+    delete temp;
+    for (int i = 0; i < n; i++){
+        det = det * m[i][i];
+    }
+    return (det / total);
+}
+
+void Matrix::outInTextFile() {
+    std::string str; std::cout << "Введите название файла записи: "; std::cin >> str;
+    std::ofstream out(str);
+
+    if (out.is_open())
+    {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colms; j++) {
+                out << m[i][j] << " ";
+            }
+            out << "\n";
+        }
+    }
+    out.close();
 }
