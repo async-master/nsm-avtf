2e6f44f79146213cc286dba06fdd015676da92bd
Nikita M <125992442+async-master@users.noreply.github.com>
2023-10-08 23:53:29 +0700

    Update main.cpp

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,22 +1,63 @@
-#include <iostream>
-#include <Matrix.h>
-
 int main()
 {
-    std::cout << "Hello World!\n";
-    int rows, colms,num;
+    
+    int n;
+    Matrix a{};
+    Matrix q{};
+    std::string fName;
+    setlocale(LC_ALL, "Russian");
     do {
-        std::cin >> rows >> colms >> num;
-        Matrix a(rows, colms);
-        a.print_size();
-        a.full_in();
-        a.print_matrix();
-        if (num != 1) {
-            a.milt_on_num(num);
-            std::cout << "\n";
-            a.print_matrix();
+        std::cout << "Что бы создать матрицу нажмите 1 \n";
+        std::cout << "Что бы умножить матрицу на число нажмите 2 \n";
+        std::cout << "Что бы транспонировать матрицу нажмите 3 \n";
+        std::cout << "Что бы найти определитель матрицы нажмите 4 \n";
+        std::cout << "Что бы вывести матрицу нажмите 5 \n";
+        std::cout << "Что бы вывести матрицу в файл нажмите 6 \n";
+        std::cout << "Что бы удалить матрицу нажмите 7 \n";
+        std::cin >> n;
+        switch (n) {
+        case 1:
+            {
+            std::cout << "Введите название файла, содержащего матрицу: ";
+            std::cin >> fName;
+            
+                Matrix b{ fName };
+                a = b;
+            };
+            break;
+        case 2:
+            std::cout << "Введите число на которое хотите умножить матрицу: \n";
+            double num; std::cin >> num;
+            a.miltOnNum(num);
+        case 3:
+            a = a.transp();
+            break;
+        case 4:
+            if (a.rtnColms() != a.rtnRows()) {
+                std::cout << "Требуется Квадратная матрица. \n \n";
+                continue; 
+            }
+            std::cout << a.det(a.rtnM(), a.rtnColms()) << "\n";
+            break;
+        case 5:
+            if (a.rtnColms() == 0) {
+                std::cout << "Сначала создайте матрицу. \n";
+            }
+            if (a.rtnColms() != 0) {
+                std::cout << "\n";
+                a.printMatrix();
+            }
+            break;
+        case 6:
+            a.outInTextFile();
+            break;
+        case 7:
+            a = q;
+            break;
+        default:
+            continue;
+            break;
         }
-
-        
-    } while (rows != 0);
+    } while (n != 0);
+    std::cout << "Программма завершена успешно. \n";
 }
