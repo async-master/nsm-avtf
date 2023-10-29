6ac83f5703ce85466855fadf11fb2d4991f07853
Nikita M <125992442+async-master@users.noreply.github.com>
2023-10-29 20:04:28 +0700

    Update Matrix.h

--- OOP/Matrix.h
+++ OOP/Matrix.h
@@ -7,12 +7,14 @@ class Matrix {
     int colms;
     std::vector<std::vector<double>> m;
     //std::vector<std::vector<double>> getCofactor(const std::vector<std::vector<double>> vect, int size);
-    void getCofactor(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp, int p, int q, int n);
-    void adjoint(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp);
+    //void getCofactor(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp, int p, int q, int n);
+    //void adjoint(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp);
 public:
     Matrix() : rows(0), colms(0), m{ 0 }
     {}
     Matrix(int rows, int colms) : rows(rows), colms(colms), m{}
+    {}
+    Matrix(int rows, int colms, std::vector<std::vector<double>> m) : rows(rows), colms(colms), m{}
     {}
     Matrix(std::string str);
 
@@ -25,7 +27,7 @@ public:
     Matrix transp();
     double det(/*std::vector<std::vector<double>> m, int rows*/);
 
-    void inverse(std::vector<std::vector<double>> vect);
+    Matrix inverse();
 
     void outInTextFile();
     void outInCSVFile();
