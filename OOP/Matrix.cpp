8cea8d7c93fbbd92c9ad93553c309f2dcc9762fa
Nikita M <125992442+async-master@users.noreply.github.com>
2023-10-29 20:04:41 +0700

    Update Matrix.cpp

--- OOP/Matrix.cpp
+++ OOP/Matrix.cpp
@@ -3,41 +3,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
-/*
-std::vector<std::vector<double>> Matrix::getCofactor(std::vector<std::vector<double>> vect, int size) {
-    if (vect.size() != vect[0].size()) {
-        throw std::runtime_error("Matrix is not quadratic");
-    }
 
-    std::vector<std::vector<double>> solution(vect.size(), std::vector<double>(vect.size()));
-    std::vector<std::vector<double>> subVect(vect.size() - 1, std::vector<double>(vect.size() - 1));
 
-    for (std::size_t i = 0; i < vect.size(); i++) {
-        for (std::size_t j = 0; j < vect[0].size(); j++) {
-
-            int p = 0;
-            for (size_t x = 0; x < vect.size(); x++) {
-                if (x == i) {
-                    continue;
-                }
-                int q = 0;
-
-                for (size_t y = 0; y < vect.size(); y++) {
-                    if (y == j) {
-                        continue;
-                    }
-
-                    subVect[p][q] = vect[x][y];
-                    q++;
-                }
-                p++;
-            }
-            solution[i][j] = pow(-1, i + j) * det(subVect, size);
-        }
-    }
-    return solution;
-}
-*/
 
 Matrix::Matrix(std::string str) {
     this->colms = 0;
@@ -90,10 +57,46 @@ Matrix Matrix::transp() {
     return temp;
 }
 
-double Matrix::det(/*std::vector<std::vector<double>> m, int n*/) {
+double Matrix::det() {
    
     std::vector<std::vector<double>> m = this->rtnM();
     int n = this->rtnColms();
+    double num1, num2, det = 1, index, total = 1;
+    double* temp{ new double[n + 1] };
+    for (int i = 0; i < n; i++)
+    {
+        index = i;
+        while (index < n && m[index][i] == 0)  index++;
+        if (index == n)continue;
+        if (index != i) {
+            for (int j = 0; j < n; j++) {
+                double temp = m[index][j];
+                m[index][j] = m[i][j];
+                m[i][j] = temp;
+            }
+            det = det * pow(-1, index - i);
+        }
+        for (int j = 0; j < n; j++) {
+            temp[j] = m[i][j];
+        }
+        for (int j = i + 1; j < n; j++) {
+            num1 = temp[i];
+            num2 = m[j][i];
+            for (int k = 0; k < n; k++) {
+                m[j][k] = (num1 * m[j][k]) - (num2 * temp[k]);
+            }
+            total = total * num1;
+        }
+    }
+    delete[] temp;
+    for (int i = 0; i < n; i++) {
+        det = det * m[i][i];
+    }
+    return det / total;
+}
+
+double detA(std::vector<std::vector<double>> m, int n) {
+
     double num1, num2, det = 1, index, total = 1;
     double* temp{ new double[n + 1] };
     for (int i = 0; i < n; i++)
@@ -129,37 +132,57 @@ double Matrix::det(/*std::vector<std::vector<double>> m, int n*/) {
     return (det / total);
 }
 
-void getCofactor(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp, int p, int q, int n) {
-    int i = 0, j = 0;
-    for (int r = 0; r < n; r++) {
-        for (int c = 0; c < n; c++) //Copy only those elements which are not in given row r and column c: {
-            if (r != p && c != q) {
-                temp[i][j++] = vect[r][c]; //If row is filled increase r index and reset c index
-                if (j == n - 1) {
-                    j = 0; i++;
+Matrix Matrix::inverse() {
+    int sizeM = this->colms;
+    int size = this->colms - 1;
+    std::vector<std::vector<double>> m = this->m;
+    int detM = detA(m, sizeM);
+    std::vector<std::vector<double>> temp;
+    std::vector<std::vector<double>> temp1;
+    temp.resize(size);
+    for (int i = 0; i < size; i++)
+        temp[i].resize(size);
+    temp1.resize(sizeM);
+    for (int i = 0; i < sizeM; i++)
+        temp1[i].resize(sizeM);
+    
+    for (int q = 0;q < sizeM; q++) {
+        for (int w = 0;w < sizeM; w++) {
+            int ii = 0;
+            int jj = 0;
+            for (int i = 0;i < sizeM;i++) {
+                if (i == q) continue;
+                for (int j = 0;j < sizeM;j++) {
+                    if (j == w) continue;
+                    
+                    temp[ii][jj] = m[i][j];
+                    
+                    jj++;
                 }
+                jj = 0;
+                ii++;
             }
-    }
-}
-
-void adjoint(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp)
-{
-    int s = 1,
-    std::vector<std::vector<double>> temp1;    
-
-    for (int i = 0; i < vect.size(); i++) {
-        for (int j = 0; j < vect.size(); j++) {
-            //To get cofactor of M[i][j]
-            getCofactor(vect, temp1, i, j, vect.size());
-            s = ((i + j) % 2 == 0) ? 1 : -1; //sign of adj[j][i] positive if sum of row and column indexes is even.
-            temp[j][i] = (s) * (det(temp1, vect.size() - 1)); //Interchange rows and columns to get the transpose of the cofactor matrix
+            temp1[q][w] = detA(temp,size)/ detM * pow(-1,q+w+2);
         }
     }
-}
-
-
-void Matrix::inverse() {
+    for (int i = 0;i < sizeM;i++) {
+        for (int j = 0;j < sizeM;j++) {
+            std::cout << temp1[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    Matrix fin(sizeM, sizeM);
+    fin.m.resize(fin.rows);
+    for (int i = 0; i < fin.rows; i++) {
+        fin.m[i].resize(fin.colms);
+        for (int j = 0; j < fin.colms; j++) {
+            fin.m[i][j] = temp1[j][i];
+        }
+    }
+    return fin;
     
+    
+    //std::cout << size << "\n";
 }
 
 void Matrix::outInTextFile() {
